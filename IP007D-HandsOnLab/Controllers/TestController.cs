d1b10b431c5b5bc0afeb8319b9a741cb351b57c8
peter <peter@DESKTOP-QFJRNE3>
2022-11-29 22:52:59 +0100

    Add project files.

---
+++ IP007D-HandsOnLab/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IP007D_HandsOnLab.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        [Route("corvinus/szerverido")]
+        public IActionResult GetPontosIdo()
+        {
+            string pontosIdo = DateTime.Now.ToShortTimeString();
+            return new ContentResult
+            {
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain,
+                Content = pontosIdo,
+            };
+        }
+    }
+}
