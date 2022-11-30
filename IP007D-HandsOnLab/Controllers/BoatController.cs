5c6d2393efb597c7bc64301d61abb716de77238b
Péter Répássy <peter.repassy@stud.uni-corvinus.hu>
2022-11-30 11:41:00 +0100

    handsonlab server

---
+++ IP007D-HandsOnLab/Controllers/BoatController.cs
@@ -0,0 +1,21 @@
+﻿using IP007D_HandsOnLab.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IP007D_HandsOnLab.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public ActionResult Kerdes()
+        {
+            HajosContext context = new HajosContext();
+            var kerdesek = from x in context.Questions select x.Question1;
+
+            return new JsonResult(kerdesek);
+        }
+    }
+}
